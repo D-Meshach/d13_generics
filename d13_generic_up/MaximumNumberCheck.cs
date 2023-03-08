5b4ec69fa891c23ebd07907480846abcb4950fbc
D-Meshach <meshachdaniel456@gmail.com>
2023-03-09 01:00:20 +0530

    maximum among float

--- d13_generic_up/MaximumNumberCheck.cs
+++ d13_generic_up/MaximumNumberCheck.cs
@@ -28,5 +28,25 @@ namespace d13_generic_up
 
 
         }
+
+        public static double MaxFloatCheck(double firstValue, double secondValue, double thirdValue)
+        {
+
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(secondValue) > 0 && thirdValue.CompareTo(firstValue) > 0)
+            {
+                return thirdValue;
+            }
+            throw new Exception("FirstNumber,Second Number , Third Number are same");
+
+
+        }
     }
 }
--- d13_generic_up/Program.cs
+++ d13_generic_up/Program.cs
@@ -10,17 +10,31 @@ namespace d13_generic_up
     {
         static void Main(string[] args)
         {
-            int result;
+            int maxInt;
+            double maxFloat;
             //To find Greatest among 3 number
             //T.C1 Max at first Position
-            result= MaximumNumberCheck.MaxNumCheck(4,3,2);
-            Console.WriteLine("The greater number is " + result);
+            maxInt= MaximumNumberCheck.MaxNumCheck(4,3,2);
+            Console.WriteLine("The greater number among 4,3,2 is " + maxInt);
             //T.C2 Max at second Position
-            result = MaximumNumberCheck.MaxNumCheck(2, 5, 3);
-            Console.WriteLine("The greater number is " + result);
+            maxInt = MaximumNumberCheck.MaxNumCheck(2, 5, 3);
+            Console.WriteLine("The greater number among 2, 5, 3 is " + maxInt);
             //T.C3 Max at third Positon
-            result = MaximumNumberCheck.MaxNumCheck(2, 6, 8);
-            Console.WriteLine("The greater number is " + result);
+            maxInt = MaximumNumberCheck.MaxNumCheck(2, 6, 8);
+            Console.WriteLine("The greater number among 2, 6, 8 is " + maxInt);
+
+            Console.WriteLine("The greatest among float values");
+            //Maximum among three float Integers
+            //T.C1 Max at first Position
+            maxFloat = MaximumNumberCheck.MaxFloatCheck(4.4, 2.2, 3.3);
+            Console.WriteLine("The greater Float among 4.4,2.2,3.3 is " + maxFloat);
+            //T.C2 Max at second Position
+            maxFloat = MaximumNumberCheck.MaxFloatCheck(4.4, 5.5, 3.3);
+            Console.WriteLine("The greater Float among 4.4, 5.5, 3.3 is " + maxFloat);
+            //T.C3 Max at third Position
+            maxFloat = MaximumNumberCheck.MaxFloatCheck(4.4, 2.2, 6.6);
+            Console.WriteLine("The greater Float among 4.4, 2.2, 6.6 is " + maxFloat);
+
 
 
 
