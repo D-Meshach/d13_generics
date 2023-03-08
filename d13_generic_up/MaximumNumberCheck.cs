c5be1c57d3bfd26f7686103278ef47b3ec8b02ba
D-Meshach <meshachdaniel456@gmail.com>
2023-03-08 23:39:41 +0530

    greatest number

---
+++ d13_generic_up/MaximumNumberCheck.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d13_generic_up
+{
+    internal class MaximumNumberCheck
+    {
+
+        public static int MaxNumCheck(int firstValue,int secondValue,int thirdValue) {
+
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue)>0) 
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(secondValue) > 0 && thirdValue.CompareTo(firstValue) > 0) 
+            {
+                return thirdValue;
+            }
+            throw new Exception("FirstNumber,Second Number , Third Number are same");
+
+
+        }
+    }
+}
---
+++ d13_generic_up/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d13_generic_up
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+               int result= MaximumNumberCheck.MaxNumCheck(2,4,2);
+            Console.WriteLine("The greater number is " + result);
+
+            
+           
+        }
+    }
+}
