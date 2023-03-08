f36a4ed072bd70a3f133273857e00e0688190006
D-Meshach <meshachdaniel456@gmail.com>
2023-03-09 01:38:17 +0530

    string comparison added

--- d13_generic_up/MaximumNumberCheck.cs
+++ d13_generic_up/MaximumNumberCheck.cs
@@ -48,5 +48,29 @@ namespace d13_generic_up
 
 
         }
+        //Compare String type 
+        public static String MaxStringCheck(String firstString, String secondString, String thirdString) 
+        {
+            if (firstString.CompareTo(secondString) > 0 && firstString.CompareTo(thirdString) > 0 ||
+                firstString.CompareTo(secondString) >= 0 && firstString.CompareTo(thirdString) > 0 ||
+                firstString.CompareTo(secondString) > 0 && firstString.CompareTo(thirdString) >= 0) 
+            {
+                return firstString;
+            }
+            if (secondString.CompareTo(firstString) > 0 && secondString.CompareTo(thirdString) > 0 ||
+                secondString.CompareTo(firstString) >= 0 && secondString.CompareTo(thirdString) > 0 ||
+                secondString.CompareTo(firstString) > 0 && secondString.CompareTo(thirdString) >= 0)
+            {
+                return secondString;
+            }
+            if (thirdString.CompareTo(firstString) > 0 && thirdString.CompareTo(secondString) > 0 ||
+                thirdString.CompareTo(firstString) >= 0 && thirdString.CompareTo(secondString) > 0 ||
+                thirdString.CompareTo(firstString) > 0 && thirdString.CompareTo(secondString) >= 0)
+            {
+                return thirdString;
+            }
+            throw new Exception("FirstString,Second String , Third String are same");
+
+        }
     }
 }
--- d13_generic_up/Program.cs
+++ d13_generic_up/Program.cs
@@ -12,6 +12,7 @@ namespace d13_generic_up
         {
             int maxInt;
             double maxFloat;
+            String maxString;
             //To find Greatest among 3 number
             //T.C1 Max at first Position
             maxInt= MaximumNumberCheck.MaxNumCheck(4,3,2);
@@ -35,7 +36,17 @@ namespace d13_generic_up
             maxFloat = MaximumNumberCheck.MaxFloatCheck(4.4, 2.2, 6.6);
             Console.WriteLine("The greater Float among 4.4, 2.2, 6.6 is " + maxFloat);
 
-
+            Console.WriteLine("The greatest among Strings");
+            //Maximum among three Strings
+            //T.C1 Max at first Position
+            maxString = MaximumNumberCheck.MaxStringCheck("c", "b", "a");
+            Console.WriteLine("The greater Float among \"a\", \"b\", \"c\" is " + maxString);
+            //T.C2 Max at second Position
+            maxString = MaximumNumberCheck.MaxStringCheck("D", "F", "E");
+            Console.WriteLine("The greater Float among \"D\", \"F\", \"E\" is " + maxString);
+            //T.C3 Max at third Position
+            maxString = MaximumNumberCheck.MaxStringCheck("J", "K", "L");
+            Console.WriteLine("The greater Float among \"J\", \"K\", \"L\" is " + maxString);
 
 
         }
